540cab89baef09e4c6edd72a3bfb75229c60bf49
adamcockell1 <adam.cockell1@gmail.com>
2023-04-08 06:40:31 -0400

    Secondary management pages and placeholder logo

---
+++ scripts/schedule.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="../style.css">
+        <title>Restaurant Database Management Website</title>
+    </head>
+    <body>
+        <?php include './connectdb.php'; ?>
+        <div>
+            <img src="../logo.png" alt="Restaurant Logo">
+        </div>
+        <h1>Employee Schedule Page</h1>
+        <hr>
+        <h2>Navigation</h2>
+        <div>
+            <a href="./managecustomers.php">
+                <button type="button">Manage Customers</button>
+            </a>
+            <a href="./manageemployees.php">
+                <button type="button">Manage Employees</button>
+            </a>
+            <a href="../restaurant.html">
+                <button type="button">Main Page</button>
+            </a>
+            <a href="./orderinfo.php">
+                <button type="button">View Order Information</button>
+            </a>
+        </div>
+    </body>
+</html>
