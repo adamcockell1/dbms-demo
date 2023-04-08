037781c4a95be61116fc279d9bd129f9bc577668
adamcockell1 <adam.cockell1@gmail.com>
2023-04-08 09:33:53 -0400

    schedule.php and field checking implemented

--- scripts/accessdb.php
+++ scripts/accessdb.php
@@ -9,17 +9,17 @@ switch($_POST["formButton"]) {
         /* Method for adding an employee to the database */
         if (isset($_POST["id"], $_POST["email"], $_POST["firstName"],
                 $_POST["lastName"], $_POST["restaurantName"])) {
-            $id = $_POST["id"];
-            $email = $_POST["email"];
-            $firstName = $_POST["firstName"];
-            $lastName = $_POST["lastName"];
-            $restaurantName = $_POST["restaurantName"];
+            $id = filter_var($_POST["id"], FILTER_SANITIZE_NUMBER_INT);
+            $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+            $firstName = filter_var($_POST["firstName"], FILTER_SANITIZE_STRING);
+            $lastName = filter_var($_POST["lastName"], FILTER_SANITIZE_STRING);
+            $restaurantName = filter_var($_POST["restaurantName"], FILTER_SANITIZE_STRING);
             try {
                 $query = $conn -> query("INSERT INTO employee VALUES
                     ('$id', '$email', '$firstName', '$lastName', '$restaurantName')");
             } catch (PDOException $err) {
                 print "Error: ".$err -> getMessage()."<br/>".
-                "<a href='../restaurant.html'>Main Page</a>";
+                    "<a href='../restaurant.html'>Main Page</a>";
                 exit();
             }
             header("location:./manageemployees.php");
@@ -34,17 +34,38 @@ switch($_POST["formButton"]) {
     case "rmvEmployee":
         /* Method for removing an employee from the database */
         if (isset($_POST["id"])) {
-            $id = $_POST["id"];
+            $id = filter_var($_POST["id"], FILTER_SANITIZE_NUMBER_INT);
             try {
                 $query = $conn -> query("DELETE FROM employee WHERE id = '$id'");
             } catch (PDOException $err) {
                 print "Error: ".$err -> getMessage()."<br/>".
-                "<a href='../restaurant.html'>Main Page</a>";
+                    "<a href='../restaurant.html'>Main Page</a>";
                 exit();
             }
             header("location:./manageemployees.php");
         } else {
             print "Error: Remove employee not successful"."<br/>".var_dump($_POST)."<br/>".
+                "<a href='../restaurant.html'>Main Page</a>";
+            exit();
+        }
+        break;
+
+    /* Case 3 */
+    case "viewSchedule":
+        /* Method for viewing an employee schedule */
+        if (isset($_POST["id"])) {
+            $id = filter_var($_POST["id"], FILTER_SANITIZE_NUMBER_INT);
+            try {
+                $query = $conn -> query("SELECT DISTINCT * FROM shift
+                    WHERE employeeId = '$id'");
+            } catch (PDOException $err) {
+                print "Error: ".$err -> getMessage()."<br/>".
+                    "<a href='../restaurant.html'>Main Page</a>";
+                exit();
+            }
+            header("location:./schedule.php");
+        } else {
+            print "Error: View schedule not successful"."<br/>".var_dump($_POST)."<br/>".
                 "<a href='../restaurant.html'>Main Page</a>";
             exit();
         }
--- scripts/schedule.php
+++ scripts/schedule.php
@@ -12,6 +12,51 @@
             <img src="../logo.png" alt="Restaurant Logo">
         </div>
         <h1>Employee Schedule Page</h1>
+        <hr style="margin-bottom: 20px">
+        <form action="./accessdb.php" method="post">
+            <div class="formdiv">
+                <label for="employeeName">Employee:</label>
+                <select type="option" name="id" id="employeeName">
+                    <?php
+                        $result = $conn -> query("SELECT DISTINCT * FROM employee");
+                        while ($row = $result -> fetch()) {
+                            echo '<option>'.$row['firstName'].' '.$row['lastName'].
+                                ' ('.$row['id'].')</option>';
+                        }
+                        $id = filter_var($_POST["employeeName"], FILTER_SANITIZE_NUMBER_INT);
+                    ?>
+                </select>
+            </div>
+            <div>
+                <button type="submit" name="formButton" value="viewSchedule"
+                    class="btn-add">View Schedule</button>
+            </div>
+        </form>
+        <div>
+            <table>
+                <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                </tr>
+                <?php
+                    $result = $conn -> query("SELECT DISTINCT * FROM shift
+                        WHERE employeeId = '$id'");
+                        echo $id;
+                    while ($row = $result -> fetch()) {
+                        echo '<tr>
+                                <td>'.$row['day'].'</td>
+                                <td>'.$row['day'].'</td>
+                                <td>'.$row['day'].'</td>
+                                <td>'.$row['day'].'</td>
+                                <td>'.$row['day'].'</td>
+                            </tr>';
+                    }
+                ?>
+            </table>
+        </div>
         <hr>
         <h2>Navigation</h2>
         <div>
